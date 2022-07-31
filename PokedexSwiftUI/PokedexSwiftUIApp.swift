c285b81c55694e89483bce5d312f930a11d947c5
aylm11 <aliylmzofficial@hotmail.com>
2022-07-31 23:55:22 +0300

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Ali YILMAZ on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
